36b8558be4488754bdcfd8c43e629865f937cc3f
divyesh <skdivyesh333@gmail.com>
2020-06-10 18:52:49 +0530

    added user_tranactions and new_transaction
    also added thelist view builer to display the transaction list

--- lib/models/transactions.dart
+++ lib/models/transactions.dart
@@ -1,1 +1,16 @@
 //transaction class
+import 'package:flutter/foundation.dart';
+
+class Transaction {
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+
+  Transaction({
+    @required this.id,
+    @required this.title,
+    @required this.amount,
+    @required this.date,
+  });
+}
